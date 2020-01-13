335ce545626513e303abf127a5f12690aca69cba
sergey <sergio_89@ukr.net>
2020-01-13 17:42:57 +0200

    fix tslint

--- src/controllers/source.maps.controller.ts
+++ src/controllers/source.maps.controller.ts
@@ -1,9 +1,8 @@
+import fs from "fs-extra";
+import path from "path";
 import { BaseController } from "./base.controller";
-import path  from "path";
-import fs from 'fs-extra'
 
-
-const SOURCE_MAP_FOLDER = process.env.SOURCE_MAP_FOLDER || "./sourceMaps"
+const SOURCE_MAP_FOLDER = process.env.SOURCE_MAP_FOLDER || "./sourceMaps";
 
 export class SourceMapsController extends BaseController {
     public initRoutes() {
@@ -11,9 +10,9 @@ export class SourceMapsController extends BaseController {
             const sourceMap = req.body;
             const scriptName = path.basename(sourceMap.file);
 
-            await fs.ensureDir(`${SOURCE_MAP_FOLDER}`)
-            
-            await fs.writeJSON(`${SOURCE_MAP_FOLDER}${path.sep}${scriptName}`, sourceMap)
+            await fs.ensureDir(`${SOURCE_MAP_FOLDER}`);
+
+            await fs.writeJSON(`${SOURCE_MAP_FOLDER}${path.sep}${scriptName}`, sourceMap);
 
             res.json({status: "saved"});
         });
