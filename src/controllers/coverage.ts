bcedb1538a14c5217d27658e13f52898b031e0da
Roman Davlyatshin <davlyatshinra@gmail.com>
2020-06-29 14:23:28 +0400

    refactor(misc): WIP remove deprecated code Remove getAstDiff from ast.service Remove getAffectedTests and getCoverage from coverage controller Clean up API routes Add json and urlencoded limit to app config Note: probe placement is broken again, fix is in the next commit

--- src/controllers/coverage.ts
+++ src/controllers/coverage.ts
@@ -1,24 +1,4 @@
-import { getAstDiff } from '../services/ast.service';
 import * as coverageService from '../services/coverage.service';
-
-export const getAffectedTests = async (req, res): Promise<any> => {
-  const { branch } = req.query;
-
-  const coverage = await coverageService.getCoverageForBuild(branch);
-  const { updated } = await getAstDiff(branch);
-
-  const methods = [];
-
-  coverage.coverage.map(it => methods.push(...it.methods));
-
-  const affectedMethods = methods.filter(it => updated.includes(it.method));
-
-  const affectedTests = [];
-
-  affectedMethods.forEach(it => affectedTests.push(...it.tests));
-
-  res.json(affectedTests);
-};
 
 export const saveTestResults = async (req, res): Promise<any> => {
   const {
@@ -31,22 +11,4 @@ export const saveTestResults = async (req, res): Promise<any> => {
   await coverageService.processTestResults(test, branch, sources, coverage);
 
   setTimeout(() => res.json({ status: `Coverage data saved. BuildId ${branch}` }), 2000);
-};
-
-export const getCoverage = async (req, res): Promise<any> => {
-  const { branch } = req.query;
-  const data = await coverageService.getCoverageForBuild(branch);
-  res.json(data);
-};
-
-export const getScopeTests = async (req, res): Promise<any> => {
-  const { uuid } = req.query;
-  const data = await coverageService.getScopeTests(uuid);
-  res.json(data);
-};
-
-export const getRisks = async (req, res): Promise<any> => {
-  const { branch } = req.query;
-  const data = await coverageService.getBuildRisks(branch);
-  res.json(data);
 };
