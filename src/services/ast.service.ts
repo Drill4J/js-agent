eb197fc459beea71c853b47c80125eb8de22a40c
Roman Davlyatshin <davlyatshinra@gmail.com>
2020-07-08 15:31:34 +0400

    chore: remove deprecated imports, files, methods Delete deprecated file "services/agent.service" Delete deprecated definitions (src/type folder) Remove unnecessary imports of ast.service Remove deprecated methods from ast.service Add comments on plugin initiation and message parsing Note: at that point ast.service & coverage.service are merely processors Both should be supplied to Test2Code plugin as external dependencies Straightforward "import" of both will get refactored in further commits

--- src/services/ast.service.ts
+++ src/services/ast.service.ts
@@ -1,7 +1,4 @@
 import * as upath from 'upath';
-/* eslint-disable import/no-unresolved */ // TODO configure local module resolution (for development purposes)
-import { AstEntity } from '@drill4j/test2code-types';
-import storage from '../storage';
 
 // TODO move type definitions to d.ts
 interface Ast {
@@ -73,22 +70,4 @@ export function formatForBackend(data) {
       })),
     };
   });
-}
-
-export async function getAst(branch = 'master'): Promise<any> {
-  const ast = await storage.getAst(branch);
-  return ast;
-}
-
-export function validateAst(ast, branch) { // TODO describe AST
-  const isAstInvalid = !ast
-    || !Array.isArray(ast.data)
-    || ast.data.length === 0;
-  if (isAstInvalid) {
-    throw new Error(`AST missing, malformed, or contains no elements! branch: ${branch}`);
-  }
-}
-
-export async function saveAst(data): Promise<any> {
-  // await storage.saveAst(data);
 }
--- src/types/ast-method.ts
+++
@@ -1,7 +0,0 @@
-export interface AstMethod {
-  params: string[];
-  name: string;
-  loc: Record<string, unknown>;
-  body: Record<string, unknown>;
-  statements: number[];
-}
