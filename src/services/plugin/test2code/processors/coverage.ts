41c3ee18d200a48d8288ebf8c4325c20f5266074
Roman Davlyatshin <davlyatshinra@gmail.com>
2020-08-14 17:50:43 +0400

    feat: class suffix formatting

--- src/services/plugin/test2code/processors/coverage.ts
+++ src/services/plugin/test2code/processors/coverage.ts
@@ -50,7 +50,7 @@ function mapCoverageToFile(coverage, file) {
     methods: [],
   };
   if (file.suffix) {
-    result.file += file.suffix;
+    result.file += ` (class ${file.suffix})`;
   }
 
   result.methods = file.methods.map(astMethod => {
