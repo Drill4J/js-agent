7be5d9097dadf70fd620ae056b9e05ad7089ffbb
Roman Davlyatshin <davlyatshinra@gmail.com>
2021-04-04 19:27:58 +0400

    refactor: split convert into multiple functions remove ranges location filtering (filtering by fn location is enough)

--- src/services/plugin/test2code/processors/coverage/convert/index.ts
+++ src/services/plugin/test2code/processors/coverage/convert/index.ts
@@ -36,15 +36,164 @@ import {
   MethodLocation,
 } from '../types';
 import { extractScriptNameFromUrl } from '../util';
-import originalToExecClass from './original-to-exec-class';
-import rawToOriginal from './raw-to-original';
-import v8ToRaw from './v8-to-raw';
 import Source from './lib/source';
 import normalizeScriptPath from '../../../../../../util/normalize-script-path';
 
 const logger = LoggerProvider.getLogger('drill', 'coverage-processor');
 
-function rangeMapSimple(range: V8CoverageRange, cache: any) {
+export default async function convert(
+  v8coverage: V8ScriptCoverage[],
+  sourceMapPath: string,
+  astEntities: AstEntity[],
+  testName: string,
+  cache: Record<string, any>,
+): Promise<ExecClassData[]> {
+  const dirDate = Date.now();
+  const prepMark = global.prf.mark('prepare');
+  await fsExtra.ensureDir(`./out/raw/${dirDate}/${testName}`);
+  await fsExtra.ensureDir(`./out/exec-class/${dirDate}`);
+  // Prepare source & source map
+  await Promise.all(
+    v8coverage.map(async v8ScriptCoverage => {
+      const { sourceHash, scriptId, functions } = v8ScriptCoverage;
+      if (cache[sourceHash]) return;
+
+      const { source: rawSource, url } = v8ScriptCoverage;
+      const source = new Source(rawSource);
+      const scriptName = extractScriptNameFromUrl(url);
+      await fsExtra.writeJSON(`./out/raw/${dirDate}/${testName}/${scriptName}.json`, { sourceHash, scriptId, functions }, { spaces: 2 });
+      // eslint-disable-next-line no-param-reassign
+      cache[sourceHash] = {
+        // FIXME SOURCE_ROOT is set in js-ast-parser config, get it from there
+        originalFilePath:
+          process.env.SKIP_SOURCE_MAPPING === 'true' &&
+          url.indexOf(process.env.SOURCE_ROOT) === 0 &&
+          url.substring(process.env.SOURCE_ROOT.length, url.length),
+        source,
+        mappings: {},
+        sourceMapConsumer:
+          process.env.SKIP_SOURCE_MAPPING !== 'true' &&
+          // FIXME                                             crawl sourcemaps contents for original  file names
+          (await new SourceMapConsumer(getSourceMap(rawSource, scriptName, sourceMapPath))),
+      };
+    }),
+  );
+  global.prf.measure(prepMark);
+
+  const mapTotalMarker = global.prf.mark('mapTotal');
+
+  // Convert ranges from absolute offsets to line:column positions
+  let coverage;
+  if (process.env.SKIP_SOURCE_MAPPING === 'true') {
+    coverage = v8coverage.map<V8ScriptCoverage>(convertOffsetsWithoutSourcemap(cache));
+  } else {
+    coverage = v8coverage.map<V8ScriptCoverage>(convertOffsetsWithSourcemap(cache));
+  }
+  global.prf.measure(mapTotalMarker);
+
+  // Map coverage onto probes for each "AST" entity (file or class)
+  const toExecTotalMarker = global.prf.mark('toExecTotal');
+  const result = astEntities.map<ExecClassData>(astEntity => {
+    const entityCoverage = coverage
+      .map(x => x.functions.filter(fn => fn.location?.source.includes(astEntity.filePath)))
+      .filter(fns => fns.length > 0);
+
+    let probes;
+    if (entityCoverage.length === 0) {
+      probes = astEntity.methods.reduce((a, method) => [...a, ...new Array(method.probes.length).fill(false)], []);
+      // probes = astEntity.methods.reduce(
+      //   (a, method) => [...a, ...method.probes.map(probe => ({ [`${probe.line}:${probe.column}`]: false }))],
+      //   [],
+      // );
+    } else {
+      /*
+        Each entityCoverage element represents coverage of file/class from
+        independent V8 isolates - completely separate instances of V8 engine.
+
+        They must be mapped onto AST entity probes __independently__.
+        
+        __After that__ mapping results can be merged to represent
+        "the whole picture" of entity coverage.
+      */
+      const probesArrays = entityCoverage.map(cov => mapCoverageToEntityProbes(astEntity, cov, cache));
+      probes = mergeArrays(probesArrays, (a, b) => a || b);
+    }
+    return {
+      id: undefined,
+      className: normalizeScriptPath(astEntity.filePath) + (astEntity.suffix ? `.${astEntity.suffix}` : ''),
+      probes,
+      testName,
+    };
+  });
+  global.prf.measure(toExecTotalMarker);
+
+  await fsExtra.writeJSON(`./out/exec-class/${dirDate}/${testName}.json`, result, { spaces: 2 });
+
+  // return [];
+  return result;
+}
+
+function convertOffsetsWithSourcemap(
+  cache: Record<string, any>,
+): (value: V8ScriptCoverage, index: number, array: V8ScriptCoverage[]) => V8ScriptCoverage {
+  return v8ScriptCoverage => {
+    const { functions, sourceHash } = v8ScriptCoverage;
+    return {
+      ...v8ScriptCoverage,
+      functions: functions
+        .map<V8FunctionCoverage>(fn => {
+          const mapMarker = global.prf.mark('mapFn');
+          const location = rangeMapWithSourcemap(fn.ranges[0], cache[sourceHash]);
+
+          // FIXME null location for the whole file range (todomvc 0 - 8929)
+          if (location?.source) {
+            if (isPrefixOmissionEnabled()) {
+              location.source = omitPrefix(location.source);
+            }
+            if (isPrefixAppendageEnabled()) {
+              location.source = appendPrefix(location.source);
+            }
+          }
+
+          const res = {
+            ...fn,
+            location,
+            ranges:
+              location?.source &&
+              fn.ranges
+                .map(range => rangeMapWithSourcemap(range, cache[sourceHash]))
+                // .filter(range => range && range.source && !range.source.includes('node_modules'))
+                // 3. Apply source path transformations if needed
+                // FIXME move conditional "up", to avoid evaluation each time ranges are mapped (it's pointless)
+                .map(R.when(isPrefixOmissionEnabled, range => ({ ...range, source: omitPrefix(range.source) })))
+                .map(R.when(isPrefixAppendageEnabled, range => ({ ...range, source: appendPrefix(range.source) }))),
+          };
+          global.prf.measure(mapMarker);
+          return res;
+        })
+        // Filter function not present in the original source files (bundler generated boilerplate & node_modules sources)
+        .filter((fn: any) => fn.location?.source && !fn.location?.source.includes('node_modules')),
+    };
+  };
+}
+
+function convertOffsetsWithoutSourcemap(
+  cache: Record<string, any>,
+): (value: V8ScriptCoverage, index: number, array: V8ScriptCoverage[]) => V8ScriptCoverage {
+  return v8ScriptCoverage => {
+    const { functions, sourceHash } = v8ScriptCoverage;
+    return {
+      ...v8ScriptCoverage,
+      functions: functions.map(fn => ({
+        ...fn,
+        location: mapRangeWithoutSourcemap(fn.ranges[0], cache[sourceHash]),
+        ranges: fn.ranges.map(range => mapRangeWithoutSourcemap(range, cache[sourceHash])),
+      })),
+    };
+  };
+}
+
+function mapRangeWithoutSourcemap(range: V8CoverageRange, cache: any) {
   const { startOffset, endOffset, count } = range;
   return {
     count,
@@ -71,161 +220,8 @@ function rangeMapWithSourcemap(range: V8CoverageRange, cache: any) {
   return {
     ...originalPosition,
     count: range.count,
+    input: range,
   };
-}
-
-export default async function convert(
-  v8coverage: V8ScriptCoverage[],
-  sourceMapPath: string,
-  astEntities: AstEntity[],
-  testName: string,
-  cache: Record<string, any>,
-): Promise<ExecClassData[]> {
-  const dirDate = Date.now();
-  await fsExtra.ensureDir(`./out/raw/${dirDate}/${testName}`);
-  await fsExtra.ensureDir(`./out/exec-class/${dirDate}`);
-  // 0. Prepare source & source map
-  await Promise.all(
-    v8coverage.map(async v8ScriptCoverage => {
-      const { sourceHash, scriptId, functions } = v8ScriptCoverage;
-      if (cache[sourceHash]) return;
-
-      const { source: rawSource, url } = v8ScriptCoverage;
-      const source = new Source(rawSource);
-      // FIXME SOURCE_ROOT is set in js-ast-parser config, get it from there
-      // that is used ONLY when coverage mapping is disabled
-      const originalFilePath = url.indexOf(process.env.SOURCE_ROOT) === 0 && url.substring(process.env.SOURCE_ROOT.length, url.length);
-
-      const scriptName = extractScriptNameFromUrl(url);
-      await fsExtra.writeJSON(`./out/raw/${dirDate}/${testName}/${scriptName}.json`, { sourceHash, scriptId, functions }, { spaces: 2 });
-
-      // eslint-disable-next-line no-param-reassign
-      cache[sourceHash] = {
-        originalFilePath,
-        source,
-        mappings: {},
-        sourceMapConsumer:
-          process.env.SKIP_SOURCE_MAPPING !== 'true' &&
-          // FIXME                                             crawl sourcemaps contents for original  file names
-          (await new SourceMapConsumer(getSourceMap(rawSource, scriptName, sourceMapPath))),
-      };
-    }),
-  );
-
-  // 1. Convert function ranges
-  let coverage1;
-  if (process.env.SKIP_SOURCE_MAPPING === 'true') {
-    // - if sourcemapping disabled - from absolute offsets to relative lines and columns
-    coverage1 = v8coverage.map<V8ScriptCoverage>(v8ScriptCoverage => {
-      const { functions, sourceHash } = v8ScriptCoverage;
-      return {
-        ...v8ScriptCoverage,
-        sourceHash,
-        functions: functions.map(fn => ({
-          ...fn,
-          location: rangeMapSimple(fn.ranges[0], cache[sourceHash]),
-          ranges: fn.ranges.map(range => rangeMapSimple(range, cache[sourceHash])),
-        })),
-      };
-    });
-  } else {
-    // - if sourcemapping enabled  - from absolute offsets in transpiled code to relative lines and columns in the original source files
-    coverage1 = v8coverage.map<V8ScriptCoverage>(v8ScriptCoverage => {
-      const { functions, sourceHash } = v8ScriptCoverage;
-      return {
-        ...v8ScriptCoverage,
-        sourceHash,
-        functions: functions
-          .map<V8FunctionCoverage>(fn => {
-            // FIXME this is horrible :(
-            const location = rangeMapWithSourcemap(fn.ranges[0], cache[sourceHash]);
-
-            // FIXME null location for the whole file range (todomvc 0 - 8929)
-            if (isPrefixOmissionEnabled() && location) {
-              location.source = omitPrefix(location.source);
-            }
-            if (isPrefixAppendageEnabled() && location) {
-              location.source = appendPrefix(location.source);
-            }
-
-            return {
-              ...fn,
-              location,
-              ranges: fn.ranges
-                .map(range => rangeMapWithSourcemap(range, cache[sourceHash]))
-                // 2. Filter ranges not present in the original source files (bundler generated boilerplate & node_modules sources)
-                .filter(range => range && range.source && !range.source.includes('node_modules'))
-                // 3. Apply source path transformations if needed
-                // FIXME move conditional "up", to avoid evaluation each time ranges are mapped (it's pointless)
-                .map(R.when(isPrefixOmissionEnabled, range => ({ ...range, source: omitPrefix(range.source) })))
-                .map(R.when(isPrefixAppendageEnabled, range => ({ ...range, source: appendPrefix(range.source) }))),
-            };
-          })
-          .filter(fn => (fn as any).location), // filter out function coverage not present in the original source
-      };
-    });
-  }
-
-  // если transpiled range --> sourcemap --> lines != original lines (from AST parser)
-  // is   transpiled range --> sourcemap --> lines == original range (from AST parser) ---> generated lines ---> original lines
-
-  // // const fnsCoverage = R.flatten(R.map(R.prop('functions'), coverage));
-  // const coverage2 = coverage1.map(x => {
-  //   return {
-  //     ...x,
-  //     functionsByLocations: x.functions.reduce((a, fn) => {
-  //       const key = locationToKey(fn.location);
-  //       if (!a[key]) {
-  //         // eslint-disable-next-line no-param-reassign
-  //         a[key] = fn;
-  //       } else {
-  //         // eslint-disable-next-line no-param-reassign
-  //         a[key].ranges = a[key].ranges.concat(fn.ranges);
-  //       }
-  //       return a;
-  //     }, {}),
-  //   };
-  // });
-
-  // 3. Map coverage to format supported by Drill4J admin backend component
-  const result = astEntities.map<ExecClassData>(astEntity => {
-    const entityCoverage = coverage1
-      .map(scriptCoverage => scriptCoverage.functions.filter(fn => fn.location?.source.includes(astEntity.filePath))) // FIXME null location
-      .filter(scriptCoverage => scriptCoverage.length > 0);
-    // const entityCoverage = coverage2.reduce(
-    //   (acc, scriptCoverage) => {
-    //     const functions = scriptCoverage.functions.filter(fn => fn.location?.source.includes(astEntity.filePath));
-    //     return [...acc, ...functions];
-    //   }, // FIXME null location
-    //   [],
-    // );
-
-    const className = normalizeScriptPath(astEntity.filePath) + (astEntity.suffix ? `.${astEntity.suffix}` : '');
-
-    let probes;
-    if (entityCoverage.length === 0) {
-      probes = astEntity.methods.reduce((a, method) => [...a, ...new Array(method.probes.length).fill(false)], []);
-      // probes = astEntity.methods.reduce(
-      //   (a, method) => [...a, ...method.probes.map(probe => ({ [`${probe.line}:${probe.column}`]: false }))],
-      //   [],
-      // );
-    } else {
-      // each entityCoverage element represents coverage from independent V8 isolates, thus they can be merged only after mapping
-      const probesArrays = entityCoverage.map(cov => mapCoverageToEntityProbes2(astEntity, cov, cache));
-      probes = mergeArrays(probesArrays, (a, b) => a || b);
-    }
-    return {
-      id: undefined,
-      className,
-      probes,
-      testName,
-    };
-  });
-
-  await fsExtra.writeJSON(`./out/exec-class/${dirDate}/${testName}.json`, result, { spaces: 2 });
-
-  // return [];
-  return result;
 }
 
 function mergeArrays(arrays, elementMerger) {
@@ -240,12 +236,7 @@ function mergeArrays(arrays, elementMerger) {
   }, arrays[0]);
 }
 
-function locationToKey(location) {
-  const { startLine, relStartCol: startCol, endLine, relEndCol: endCol } = location;
-  return `${startLine}/${startCol}-${endLine}/${endCol}`;
-}
-
-function mapCoverageToEntityProbes2(entity: AstEntity, entityCoverage, cache: any) {
+function mapCoverageToEntityProbes(entity: AstEntity, entityCoverage, cache: any) {
   return entity.methods.reduce((result, method) => {
     // HACK because estree parser yields end position as position for character AFTER the node, even if there are no characters ahead
     //      that causes a root node to have the end pos residing at an empty line (with no characters at all)
@@ -257,12 +248,8 @@ function mapCoverageToEntityProbes2(entity: AstEntity, entityCoverage, cache: an
     );
     // if (methodCoverage.length === 0) return [...result, ...method.probes.map(probe => ({ [`${probe.line}:${probe.column}`]: false }))];
     if (methodCoverage.length === 0) return [...result, ...new Array(method.probes.length).fill(false)];
-    // eslint-disable-next-line no-param-reassign
 
-    // FIXME reducing might lead to errors when methodCoverage has more than one item
-    // THIS IS NOT RIGHT! If method ranges included single not covered range, that's gonna break
     const notCoveredRanges = methodCoverage.reduce((acc, part) => [...acc, ...part.ranges.filter(range => range.count === 0)], []);
-    // const notCoveredRanges = methodCoverage.reduce((acc, part) => [...acc, ...part.ranges.slice(1, part.ranges.length)], []);
     return method.probes.reduce((acc, probe) => {
       acc.push(notCoveredRanges.findIndex(range => isProbeInsideRange(probe, range)) === -1);
       // acc.push({ [`${probe.line}:${probe.column}`]: notCoveredRanges.findIndex(range => isProbeInsideRange(probe, range)) === -1 });
